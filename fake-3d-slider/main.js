149e69231402334084ee0b02613a03f5206de5c1
WindyWin <19521362@gm.uit.edu.vn>
2023-12-14 16:48:47 +0700

    keep up learning

---
+++ fake-3d-slider/main.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body")
+
+body.addEventListener("scroll", (e)=>{
+    console.log(e)
+})
---
+++ fake-3d-slider/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      "display": "Clash Display Variable"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
